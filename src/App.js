49c2cc3746306887575d75d96bdde72b5df2a428
MarkelWhit <litslayer@gmail.com>
2021-05-27 12:54:18 -0500

    tried

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './index.js'
+
+
+
 function App() {
-  return (
+ const [image, setImage] = useState()
+ const [events, setEvents] = useEffect([])
+ //const [] = useState(null)
+
+ useEffect(() => {
+axios.get(`${BASE_URL}/planetary/${API_KEY}`)
+  .then(res => {
+    console.log(res)
+    setEvents(res.data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}, [])
+
+
+
+return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <div id='image'>
+        <img src=''/> 
+      </div> 
+
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { render } from 'react'
 import "./index.css";
 import App from "./App";
-
+export const BASE_URL = 'https://api.nasa.gov'
+export const API_KEY = '/apod'
 ReactDOM.render(<App />, document.getElementById("root"));
